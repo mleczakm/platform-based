64b001684a97e1435116301fe7aa776cdbe4d6d2
Michał Mleczko <kontakt@michalmleczko.waw.pl>
2017-02-26 21:19:24 +0100

    Add command exist method

--- src/PlatformBased/Executor.php
+++ src/PlatformBased/Executor.php
@@ -18,4 +18,30 @@ class Executor
 
         return $returnValue;
     }
+
+    public function commandExist($command)
+    {
+        $whereIsCommand = (PHP_OS == 'WINNT') ? 'where' : 'which';
+
+        $process = proc_open(
+            "$whereIsCommand $command",
+            array(
+                0 => array("pipe", "r"), //STDIN
+                1 => array("pipe", "w"), //STDOUT
+                2 => array("pipe", "w"), //STDERR
+            ),
+            $pipes
+        );
+        if ($process !== false) {
+            $stdout = stream_get_contents($pipes[1]);
+            $stderr = stream_get_contents($pipes[2]);
+            fclose($pipes[1]);
+            fclose($pipes[2]);
+            proc_close($process);
+
+            return $stdout != '';
+        }
+
+        return false;
+    }
 }
